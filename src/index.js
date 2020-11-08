bce25bb1c5de956d7298e14296d8831d0001bb29
jesusCracker <644392839@qq.com>
2020-11-08 21:40:13 +0800

    init react-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from "react-redux";
+import rootReducers from "./reducers";
+import logger from 'redux-logger';
+import  {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from "redux-thunk";
+import promise from 'redux-promise-middleware'
+
+
+const store=createStore(rootReducers,{},composeWithDevTools(applyMiddleware(logger,thunk,promise)));
 
 ReactDOM.render(
-  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Provider>
+  ,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
